d8f16cfa8eeab4e925892abd748c444099a90070
paipeter <you@example.com>
2020-08-14 10:32:55 +0800

    temp

--- index.js
+++
@@ -1,8 +0,0 @@
-var exec = require('child_process').exec;
-
-var port = process.env.PORT || 4000;
-var hexoServerProcess = exec("hexo server -p " + port);
-
-hexoServerProcess.stdout.on('data', function(data) {
-    console.log(data.toString()); 
-});
--- source/_posts/TEST.md
+++
@@ -1,8 +0,0 @@
-title: TEST
-author: Peter Pai
-date: 2020-08-13 17:00:10
-tags:
----
-- TEST
-
-
